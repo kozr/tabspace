655c420f88cad3470804defef830c6ccbc4cc00e
kozr <zxzflow@gmail.com>
2022-10-06 00:47:47 -0700

    custom time

--- src/utils/ModdedChrono.ts
+++ src/utils/ModdedChrono.ts
@@ -6,12 +6,13 @@ const moddedChrono = chrono.casual.clone();
 moddedChrono.refiners.push({
   refine: (context: ParsingContext, results: ParsingResult[]) => {
     // If there is no AM/PM (meridiem) specified,
-    // usually PM
+    //  let all time be 12:00 AM.
     results.forEach((result) => {
-      const hour = result.start.get('hour') || 0
-      if (!result.start.isCertain('meridiem') && hour >= 1 && hour <= 5) {
-        result.start.assign('meridiem', 1);
-        result.start.assign('hour', hour + 12);
+      if (!result.start.isCertain("meridiem")) {
+        result.start.assign("meridiem", 0);
+        result.start.assign("hour", 0);
+        result.start.assign("minute", 0);
+        result.start.assign("second", 0);
       }
     });
     return results;
